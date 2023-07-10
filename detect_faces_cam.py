66dd5081d89524552ccf4f84342c71a95fe4c451
NguyenThanhAI <Thanh.CEAA.K58.HUST@gmail.com>
2023-07-10 10:26:30 +0700

    Add compute head pose

--- detect_faces_cam.py
+++ detect_faces_cam.py
@@ -8,10 +8,14 @@ from mtcnn import MTCNN
 from mtcnn_pytorch.src.visualization_utils import show_bboxes
 
 from retinaface.RetinaFace import RetinaModel
+import face_alignment
 
 from videostream.videostream import QueuedStream
 
+from face_tracking.face_detection import FaceDetection
+from face_tracking.face_tracker import FaceTracker
 
+from utils import face_orientation, compute_head_pose, alignment_procedure
 
 
 if __name__ == "__main__":
@@ -21,14 +25,19 @@ if __name__ == "__main__":
         os.makedirs(save_dir, exist_ok=True)
     face_id = 0
     
-    extend = 0.1
+    extend = 0.0
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
     #mtcnn = MTCNN()
-    retina = RetinaModel()
+    #retina = RetinaModel()
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    fa = face_alignment.FaceAlignment(landmarks_type=face_alignment.LandmarksType.TWO_D, device=device, face_detector="sfd")
 
     stream = QueuedStream(uri=[], drop=True, fps=25)
-
+    tracker = FaceTracker(inactive_steps_before_removed=20,
+                          reid_iou_threshold=0.6,
+                          max_traject_steps=30,
+                          tentative_steps_before_accepted=3)
     stream.start()
 
     while True:
@@ -37,23 +46,85 @@ if __name__ == "__main__":
         if not ret:
             break
         
-        resp = retina.detect_faces(frame)
+        # resp = retina.detect_faces(frame)
+        # print(resp)
+        # faces = retina.extract_faces(frame, obj=resp)
         
-        faces = retina.extract_faces(frame, obj=resp)
         
         # image = Image.fromarray(np.uint8(frame)).convert('RGB')
         # bboxes, landmarks = mtcnn.detect_faces(image)
         
-        # bboxes, faces = mtcnn.align_intermediate(img=image, boxes=bboxes, landmarks=landmarks)
+        #bboxes, faces = mtcnn.align_intermediate(img=image, boxes=bboxes, landmarks=landmarks)
         
-        for face in faces:
-            face_id += 1
-            face_img = np.asarray(face)
-            cv2.imwrite(os.path.join(save_dir, "face_{}.jpg".format(face_id)), face_img)
+        dets = fa.get_landmarks(frame, return_bboxes=True, return_landmark_score=True)
+        #print("det: {}".format(dets))
+        bboxes = dets[2]
+        landmarks = dets[0]
+        #print("landmarks: {}".format(len(landmarks)))        
+        # for face in faces:
+        #     face_id += 1
+        #     face_img = np.asarray(face)
+        #     cv2.imwrite(os.path.join(save_dir, "face_{}.jpg".format(face_id)), face_img)
         
         # #print(bboxes)
         # draw_img = show_bboxes(img=image, bounding_boxes=bboxes, facial_landmarks=landmarks)
         # frame = np.asarray(draw_img)
+        
+        # try:
+        detections_list = []
+        for i, (landmark, bbox) in enumerate(zip(landmarks, bboxes)):
+            #print(bbox.shape)
+            x_1, y_1, x_2, y_2, prob = bbox
+            if prob > 0.8:
+                w = x_2 - x_1
+                h = y_2 - y_1
+                # new_x_1 = max(x_1 - extend * w, 0)
+                # new_x_2 = min(x_2 + extend * w, frame.shape[1] - 1)
+                # new_y_1 = max(y_1 - extend * h, 0)
+                # new_y_2 = min(y_2 + extend * h, frame.shape[0] - 1)
+                # cv2.rectangle(frame, (int(new_x_1), int(new_y_1)), (int(new_x_2), int(new_y_2)), color=(255, 255, 0), thickness=1)
+                detections_list.append(FaceDetection(bbox=bbox, detection_id=i))
+                #print("landmark: {}".format(landmark.shape))
+                roll, pitch, yaw = face_orientation(frame=frame, landmark=landmark)
+                #roll, pitch, yaw = compute_head_pose(landmark=landmark)
+                print("roll: {}, pitch: {}, yaw: {}".format(roll, pitch, yaw))
+                #print(y_1, y_2, x_1, x_2)
+                face_img = frame[int(y_1):int(y_2), int(x_1):int(x_2)]
+                right_eye = landmark[45]
+                left_eye = landmark[36]
+                nose = landmark[30]
+                face_img = alignment_procedure(img=face_img, left_eye=left_eye, right_eye=right_eye, nose=nose)
+                face_id += 1
+                cv2.imwrite(os.path.join(save_dir, "face_{}.jpg".format(face_id)), face_img)
+                for j, point in enumerate(landmark):
+                    if j in [8, 30, 36, 45, 48, 54]:
+                        # 8: Cằm
+                        # 30: Mũi
+                        # 36: Mắt trái
+                        # 45: Mắt phải
+                        # 48: Miệng trái
+                        # 54: Miệng phải
+                        cv2.circle(frame, (int(point[0]), int(point[1])), 2, (0, 0, 255), -1)
+                        cv2.putText(frame, str(j), (int(point[0]) + 3, int(point[1]) - 3),  cv2.FONT_HERSHEY_COMPLEX, 0.3, (255, 255, 0), 1)            
+        tracker.step(face_detections=detections_list)
+        face_tracks = tracker.get_result()
+        for face_track in face_tracks:
+            bbox = face_track.bbox
+            #print(bbox)
+            track_id = face_track.track_id
+            x_min, y_min, x_max, y_max, prob = bbox
+            x_min = int(x_min)
+            x_max = int(x_max)
+            y_min = int(y_min)
+            y_max = int(y_max)
+            print(x_min, y_min, x_max, y_max, track_id)
+            result = "Track id: {}".format(track_id)
+            cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), (255, 255, 0), 1)
+            cv2.putText(frame, result, (x_min + int(0.05*(x_max - x_min)), y_min - int(0.05*(y_max - y_min))), cv2.FONT_HERSHEY_COMPLEX, 0.3, (255, 255, 0), 1)
+                
+        # except Exception as e:
+        #     print("Exception: {}".format(e))
+        #     continue
         
         cv2.imshow("", frame)
         cv2.waitKey(1)
--- utils.py
+++ utils.py
@@ -1,6 +1,10 @@
 import os
 from typing import List
 
+import math
+
+from PIL import Image
+
 import numpy as np
 import cv2
 
@@ -25,3 +29,162 @@ def preprocess(img: np.ndarray):
     img = np.transpose(img, (2, 0, 1))
     
     return img
+
+
+def face_orientation(frame: np.ndarray, landmark):
+    size = frame.shape
+    
+    model_points = np.array([
+                            (0.0, 0.0, 0.0),             # Nose tip
+                            (0.0, -330.0, -65.0),        # Chin
+                            (-165.0, 170.0, -135.0),     # Left eye left corner
+                            (165.0, 170.0, -135.0),      # Right eye right corne
+                            (-150.0, -150.0, -125.0),    # Left Mouth corner
+                            (150.0, -150.0, -125.0)      # Right mouth corner                         
+                        ])
+    
+    image_points = np.array([
+                            (landmark[30][0], landmark[30][1]),     # Nose tip
+                            (landmark[8][0], landmark[8][1]),   # Chin
+                            (landmark[45][0], landmark[45][1]),     # Left eye left corner
+                            (landmark[36][0], landmark[36][1]),     # Right eye right corne
+                            (landmark[48][0], landmark[48][1]),     # Left Mouth corner
+                            (landmark[54][0], landmark[54][1])      # Right mouth corner
+                        ], dtype="double")
+    
+    center = (size[1]/2, size[0]/2)
+    focal_length = center[0] / np.tan(60/2 * np.pi / 180)
+    camera_matrix = np.array(
+                         [[focal_length, 0, center[0]],
+                         [0, focal_length, center[1]],
+                         [0, 0, 1]], dtype = "double"
+                         )
+    
+    dist_coeffs = np.zeros((4,1)) # Assuming no lens distortion
+    (success, rotation_vector, translation_vector) = cv2.solvePnP(model_points, image_points, camera_matrix, dist_coeffs, flags=cv2.SOLVEPNP_ITERATIVE)
+    
+    axis = np.float32([[500,0,0], 
+                          [0,500,0], 
+                          [0,0,500]])
+                          
+    imgpts, jac = cv2.projectPoints(axis, rotation_vector, translation_vector, camera_matrix, dist_coeffs)
+    modelpts, jac2 = cv2.projectPoints(model_points, rotation_vector, translation_vector, camera_matrix, dist_coeffs)
+    rvec_matrix = cv2.Rodrigues(rotation_vector)[0]
+
+    proj_matrix = np.hstack((rvec_matrix, translation_vector))
+    eulerAngles = cv2.decomposeProjectionMatrix(proj_matrix)[6] 
+
+    
+    pitch, yaw, roll = [math.radians(_) for _ in eulerAngles]
+
+
+    pitch = math.degrees(math.asin(math.sin(pitch)))
+    roll = -math.degrees(math.asin(math.sin(roll)))
+    yaw = math.degrees(math.asin(math.sin(yaw)))
+    
+    return roll, pitch, yaw
+
+
+def compute_head_pose(landmark):
+    # Facial landmark indices for relevant points
+    left_eye_indices = list(range(36, 42))
+    right_eye_indices = list(range(42, 48))
+    nose_tip_index = 30
+    mouth_center_indices = [48, 54]
+
+    # Calculate eye centers
+    left_eye_center = np.mean(landmark[left_eye_indices], axis=0)
+    right_eye_center = np.mean(landmark[right_eye_indices], axis=0)
+
+    # Compute roll angle
+    dx = right_eye_center[0] - left_eye_center[0]
+    dy = right_eye_center[1] - left_eye_center[1]
+    roll_angle = math.atan2(dy, dx) * 180.0 / math.pi
+
+    # Compute pitch angle
+    dx = left_eye_center[0] - landmark[nose_tip_index][0]
+    dy = left_eye_center[1] - landmark[nose_tip_index][1]
+    pitch_angle = math.atan2(dy, dx) * 180.0 / math.pi
+
+    # Compute yaw angle
+    dx = left_eye_center[0] - landmark[mouth_center_indices[0]][0]
+    dy = left_eye_center[1] - landmark[mouth_center_indices[0]][1]
+    yaw_angle = math.atan2(dy, dx) * 180.0 / math.pi
+
+    return roll_angle, pitch_angle, yaw_angle
+
+
+def findEuclideanDistance(source_representation, test_representation):
+    euclidean_distance = source_representation - test_representation
+    euclidean_distance = np.sum(np.multiply(euclidean_distance, euclidean_distance))
+    euclidean_distance = np.sqrt(euclidean_distance)
+    return euclidean_distance
+
+#this function copied from the deepface repository: https://github.com/serengil/deepface/blob/master/deepface/commons/functions.py
+def alignment_procedure(img, left_eye, right_eye, nose):
+
+    #this function aligns given face in img based on left and right eye coordinates
+
+    #left eye is the eye appearing on the left (right eye of the person)
+    #left top point is (0, 0)
+
+    left_eye_x, left_eye_y = left_eye
+    right_eye_x, right_eye_y = right_eye
+
+    #-----------------------
+    #decide the image is inverse
+
+    center_eyes = (int((left_eye_x + right_eye_x) / 2), int((left_eye_y + right_eye_y) / 2))
+    
+    if False:
+
+        img = cv2.circle(img, (int(left_eye[0]), int(left_eye[1])), 2, (0, 255, 255), 2)
+        img = cv2.circle(img, (int(right_eye[0]), int(right_eye[1])), 2, (255, 0, 0), 2)
+        img = cv2.circle(img, center_eyes, 2, (0, 0, 255), 2)
+        img = cv2.circle(img, (int(nose[0]), int(nose[1])), 2, (255, 255, 255), 2)
+
+    #-----------------------
+    #find rotation direction
+
+    if left_eye_y > right_eye_y:
+        point_3rd = (right_eye_x, left_eye_y)
+        direction = -1 #rotate same direction to clock
+    else:
+        point_3rd = (left_eye_x, right_eye_y)
+        direction = 1 #rotate inverse direction of clock
+
+    #-----------------------
+    #find length of triangle edges
+
+    a = findEuclideanDistance(np.array(left_eye), np.array(point_3rd))
+    b = findEuclideanDistance(np.array(right_eye), np.array(point_3rd))
+    c = findEuclideanDistance(np.array(right_eye), np.array(left_eye))
+
+    #-----------------------
+
+    #apply cosine rule
+
+    if b != 0 and c != 0: #this multiplication causes division by zero in cos_a calculation
+
+        cos_a = (b*b + c*c - a*a)/(2*b*c)
+        
+        #PR15: While mathematically cos_a must be within the closed range [-1.0, 1.0], floating point errors would produce cases violating this
+        #In fact, we did come across a case where cos_a took the value 1.0000000169176173, which lead to a NaN from the following np.arccos step
+        cos_a = min(1.0, max(-1.0, cos_a))
+        
+        
+        angle = np.arccos(cos_a) #angle in radian
+        angle = (angle * 180) / math.pi #radian to degree
+
+        #-----------------------
+        #rotate base image
+
+        if direction == -1:
+            angle = 90 - angle
+
+        img = Image.fromarray(img)
+        img = np.array(img.rotate(direction * angle).resize(size=(112, 112)))
+
+    #-----------------------
+
+    return img #return img anyway
