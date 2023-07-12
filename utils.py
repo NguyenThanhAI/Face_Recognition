231436f3a1f3e26b4cf95fcf16d3f9f8a21d9b77
NguyenThanhAI <Thanh.CEAA.K58.HUST@gmail.com>
2023-07-12 17:32:08 +0700

    Add align

--- utils.py
+++ utils.py
@@ -3,11 +3,20 @@ from typing import List
 
 import math
 
-from PIL import Image
+from io import BytesIO
 
+from PIL import Image
+import PIL
 import numpy as np
 import cv2
 
+
+def read_image(image_encoded: bytes, mode="RGB") -> np.ndarray:
+    pil_image = Image.open(BytesIO(image_encoded))
+    pil_image.convert(mode)
+    #img = np.array(pil_image)
+
+    return pil_image
 
 def enumerate_images(data_dir: str) -> List[str]:
     file_list = []
@@ -45,7 +54,7 @@ def face_orientation(frame: np.ndarray, landmark):
     
     image_points = np.array([
                             (landmark[30][0], landmark[30][1]),     # Nose tip
-                            (landmark[8][0], landmark[8][1]),   # Chin
+                            (landmark[8][0], landmark[8][1]),       # Chin
                             (landmark[45][0], landmark[45][1]),     # Left eye left corner
                             (landmark[36][0], landmark[36][1]),     # Right eye right corne
                             (landmark[48][0], landmark[48][1]),     # Left Mouth corner
@@ -188,3 +197,81 @@ def alignment_procedure(img, left_eye, right_eye, nose):
     #-----------------------
 
     return img #return img anyway
+
+
+def align_face(src_file, face_landmarks, output_size=1024, transform_size=4096, enable_padding=True):
+
+
+        lm = np.array(face_landmarks)
+        lm_chin          = lm[0  : 17]  # left-right
+        lm_eyebrow_left  = lm[17 : 22]  # left-right
+        lm_eyebrow_right = lm[22 : 27]  # left-right
+        lm_nose          = lm[27 : 31]  # top-down
+        lm_nostrils      = lm[31 : 36]  # top-down
+        lm_eye_left      = lm[36 : 42]  # left-clockwise
+        lm_eye_right     = lm[42 : 48]  # left-clockwise
+        lm_mouth_outer   = lm[48 : 60]  # left-clockwise
+        lm_mouth_inner   = lm[60 : 68]  # left-clockwise
+
+        # Calculate auxiliary vectors.
+        eye_left     = np.mean(lm_eye_left, axis=0)
+        eye_right    = np.mean(lm_eye_right, axis=0)
+        eye_avg      = (eye_left + eye_right) * 0.5
+        eye_to_eye   = eye_right - eye_left
+        mouth_left   = lm_mouth_outer[0]
+        mouth_right  = lm_mouth_outer[6]
+        mouth_avg    = (mouth_left + mouth_right) * 0.5
+        eye_to_mouth = mouth_avg - eye_avg
+
+        # Choose oriented crop rectangle.
+        x = eye_to_eye - np.flipud(eye_to_mouth) * [-1, 1]
+        x /= np.hypot(*x)
+        x *= max(np.hypot(*eye_to_eye) * 2.0, np.hypot(*eye_to_mouth) * 1.8)
+        y = np.flipud(x) * [-1, 1]
+        c = eye_avg + eye_to_mouth * 0.1
+        quad = np.stack([c - x - y, c - x + y, c + x + y, c + x - y])
+        qsize = np.hypot(*x) * 2
+
+        # Load in-the-wild image.
+        if not os.path.isfile(src_file):
+            print('\nCannot find source image. Please run "--wilds" before "--align".')
+            return
+        img = PIL.Image.open(src_file)
+
+        # Shrink.
+        shrink = int(np.floor(qsize / output_size * 0.5))
+        if shrink > 1:
+            rsize = (int(np.rint(float(img.size[0]) / shrink)), int(np.rint(float(img.size[1]) / shrink)))
+            img = img.resize(rsize, PIL.Image.ANTIALIAS)
+            quad /= shrink
+            qsize /= shrink
+
+        # Crop.
+        border = max(int(np.rint(qsize * 0.1)), 3)
+        crop = (int(np.floor(min(quad[:,0]))), int(np.floor(min(quad[:,1]))), int(np.ceil(max(quad[:,0]))), int(np.ceil(max(quad[:,1]))))
+        crop = (max(crop[0] - border, 0), max(crop[1] - border, 0), min(crop[2] + border, img.size[0]), min(crop[3] + border, img.size[1]))
+        if crop[2] - crop[0] < img.size[0] or crop[3] - crop[1] < img.size[1]:
+            img = img.crop(crop)
+            quad -= crop[0:2]
+
+        # Pad.
+        #pad = (int(np.floor(min(quad[:,0]))), int(np.floor(min(quad[:,1]))), int(np.ceil(max(quad[:,0]))), int(np.ceil(max(quad[:,1]))))
+        #pad = (max(-pad[0] + border, 0), max(-pad[1] + border, 0), max(pad[2] - img.size[0] + border, 0), max(pad[3] - img.size[1] + border, 0))
+        #if enable_padding and max(pad) > border - 4:
+        #    pad = np.maximum(pad, int(np.rint(qsize * 0.3)))
+        #    img = np.pad(np.float32(img), ((pad[1], pad[3]), (pad[0], pad[2]), (0, 0)), 'reflect')
+        #    h, w, _ = img.shape
+        #    y, x, _ = np.ogrid[:h, :w, :1]
+        #    mask = np.maximum(1.0 - np.minimum(np.float32(x) / pad[0], np.float32(w-1-x) / pad[2]), 1.0 - np.minimum(np.float32(y) / pad[1], np.float32(h-1-y) / pad[3]))
+        #    blur = qsize * 0.02
+        #    img += (scipy.ndimage.gaussian_filter(img, [blur, blur, 0]) - img) * np.clip(mask * 3.0 + 1.0, 0.0, 1.0)
+        #    img += (np.median(img, axis=(0,1)) - img) * np.clip(mask, 0.0, 1.0)
+        #    img = PIL.Image.fromarray(np.uint8(np.clip(np.rint(img), 0, 255)), 'RGB')
+        #    quad += pad[:2]
+
+        # Transform.
+        img = img.transform((transform_size, transform_size), PIL.Image.QUAD, (quad + 0.5).flatten(), PIL.Image.BILINEAR)
+        if output_size < transform_size:
+            img = img.resize((output_size, output_size), PIL.Image.ANTIALIAS)
+
+        return cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
